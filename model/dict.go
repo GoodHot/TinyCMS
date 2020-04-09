3f459336b7352e46b8c2a2c767774122e30c6114
wang22 <pause22@163.com>
2020-04-09 16:34:51 +0800

    今日功能完毕

--- model/dict.go
+++ model/dict.go
@@ -7,19 +7,20 @@ import (
 type DictType string
 
 const (
-	DictTypeText     = "text"
-	DictTypeTextArea = "textarea"
-	DictTypeSelect   = "select"
+	DictTypeText        = "text"
+	DictTypeTextArea    = "textarea"
+	DictTypeSelect      = "select" // 格式为 key:value,key:value
+	DictTypeUploadImage = "uploadImage"
 )
 
 type Dict struct {
 	orm.Model
-	Key         string   `json:"key"`
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Type        DictType `json:"type"`
-	Options     string   `json:"options"`
-	Value       string   `json:"value"`
-	ReadOnly    bool     `json:"read_only"`
-	Visible     bool     `json:"visible"`
+	Key         string   `json:"key"`         // 唯一Key
+	Name        string   `json:"name"`        // 名称
+	Description string   `json:"description"` // 描述
+	Type        DictType `json:"type"`        // 数据类型
+	Options     string   `json:"options"`     // 选项
+	Value       string   `json:"value"`       // 值
+	ReadOnly    bool     `json:"read_only"`   // 是否只读
+	Visible     bool     `json:"visible"`     // 是否可见
 }
