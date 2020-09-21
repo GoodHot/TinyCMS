243aa975cd83db03ba9009225a15075d28aea362
小王王 <pause22@163.com>
2020-09-21 14:37:42 +0800

    还存在问题，晚上处理

--- core/config.go
+++ core/config.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"reflect"
@@ -27,37 +26,74 @@ func (cfg *Config) ReadConfig(cfgPath string) error {
 	if err != nil {
 		return err
 	}
-	cfg.replaceValue()
+	err = cfg.replaceValue(cfg.cfgMap)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func (cfg *Config) replaceValue() error {
-	for k, v := range cfg.cfgMap {
-		fmt.Println(k, v)
-		//vt := reflect.TypeOf(v)
-		//if vt.Kind().String() == "string" {
-		//	if has, repKey := cfg.hasReplace(v.(string)); has {
-		//		fmt.Println(k, v, repKey)
-		//		data, exists := cfg.Get(repKey)
-		//		if !exists {
-		//			return errors.New(fmt.Sprintf("%v is not exists"))
-		//		}
-		//
-		//	}
-		//}
+func (cfg *Config) replaceValue(cfgMap map[string]interface{}) error {
+	for key, val := range cfgMap {
+		vt := reflect.TypeOf(val)
+		fmt.Println(key, vt.Kind())
+		if vt.Kind().String() == "map" {
+			cfg.replaceValue(val.(map[string]interface{}))
+			continue
+		}
+		if vt.Kind().String() == "string" {
+			has, reps := cfg.hasReplace(val.(string))
+			if !has {
+				continue
+			}
+			for _, str := range reps {
+				
+			}
+		}
 	}
+	return nil
 }
 
-func (cfg *Config) hasReplace(value string) (has bool, key string) {
+//func (cfg *Config) replaceValueByKey(key string) error {
+//value, exists := cfg.Get(key)
+//if !exists {
+//	return errors.New(fmt.Sprintf("%v is not exists", key))
+//}
+//if hasReplace, repKey := cfg.hasReplace(value); hasReplace {
+//	data, ok := cfg.Get(repKey)
+//	if !ok {
+//		return errors.New(fmt.Sprintf("%v is not exists", repKey))
+//	}
+//	if hr, _ := cfg.hasReplace(data.(string)); hr {
+//		cfg.replaceValueByKey(repKey, cfgMap)
+//	}
+//	cfgMap[key] = hasReplaceRegex.ReplaceAllString(cfgMap[key].(string), data.(string))
+//}
+//return nil
+//}
+
+func (cfg *Config) hasReplace(value string) (has bool, key []string) {
 	vals := hasReplaceRegex.FindAllStringSubmatch(value, -1)
 	if len(vals) == 0 {
-		return false, ""
+		return false, []string{}
 	}
-	key = vals[0][1]
-	has = key != ""
+	key = vals[0]
+	has = len(key) > 0
 	return
 }
 
-func (*Config) Get(key string) (string, bool) {
-	return "", false
-}
+//func (cfg *Config) Get(key string) (value interface{}, exists bool, dataType string) {
+//	keys := strings.Split(key, ".")
+//	getValue := func(tmpMap map[string]interface{}) (value interface{}, exists bool, dataType string) {
+//		length := len(keys) - 1
+//		for i, k := range keys {
+//			val := tmpMap[k]
+//			tv := reflect.TypeOf(val)
+//			if length == i {
+//				return val, true, tv.String()
+//			}
+//		}
+//		return nil, false, ""
+//	}
+//	return getValue(cfg.cfgMap)
+//}
