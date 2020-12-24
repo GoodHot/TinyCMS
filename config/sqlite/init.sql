f3c116265d0ba4a5aa808b8d6f1635b80b1f191f
DESKTOP-7RAI0UV\GuoAnYinZhang <pause22@163.com>
2020-12-24 16:04:42 +0800

    hide sql

--- config/sqlite/init.sql
+++ config/sqlite/init.sql
@@ -1,18 +1,18 @@
-CREATE TABLE "t_admin" (
-  "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "username" TEXT,
-  "email" TEXT,
-  "password" TEXT
-);
-
-CREATE TABLE "t_channel" (
-  "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "name" TEXT,
-  "path" TEXT,
-  "avatar" TEXT,
-  "visibility" integer,
-  "meta_title" TEXT,
-  "meta_description" TEXT,
-  "sort" integer,
-  "parent_id" INTEGER
-);
+-- CREATE TABLE "t_admin" (
+--   "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+--   "username" TEXT,
+--   "email" TEXT,
+--   "password" TEXT
+-- );
+--
+-- CREATE TABLE "t_channel" (
+--   "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+--   "name" TEXT,
+--   "path" TEXT,
+--   "avatar" TEXT,
+--   "visibility" integer,
+--   "meta_title" TEXT,
+--   "meta_description" TEXT,
+--   "sort" integer,
+--   "parent_id" INTEGER
+-- );
