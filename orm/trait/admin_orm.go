02c282aefd300149c2e93a71c5336832abefe670
小王王 <pause22@163.com>
2020-12-30 22:16:14 +0800

    增加角色和昵称字段

--- orm/trait/admin_orm.go
+++ orm/trait/admin_orm.go
@@ -2,11 +2,22 @@ package trait
 
 import "github.com/GoodHot/TinyCMS/core"
 
+type RoleType int
+
+const (
+	RoleTypeOwner  RoleType = 1
+	RoleTypeAdmin  RoleType = 2
+	RoleTypeEditor RoleType = 3
+	RoleTypeAuthor RoleType = 4
+)
+
 type Admin struct {
 	BaseORM
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"-"`
+	Nickname string   `json:"nickname"`
+	Username string   `json:"username"`
+	Email    string   `json:"email"`
+	Password string   `json:"-"`
+	Role     RoleType `json:"role"`
 }
 
 type AdminORM interface {
